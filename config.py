9f5eec8e8e1138f3012b868359f6c05c427efa5b
Victor Saraiva <victoralex07062005@gmail.com>
2025-11-07 14:35:39 -0300

    Atualiza os nomes dos diretórios para maior clareza.

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 
 BASE_DIR = os.path.dirname(__file__)
 DB_PATH = os.path.join(BASE_DIR, 'run.db')
-ORIGINAL_PHOTOS_DIR = os.path.join(BASE_DIR, 'dados', 'fotos_originais')
-SEGMENTED_PHOTOS_DIR = os.path.join(BASE_DIR, 'dados', 'fotos_segmentadas')
+ORIGINAL_PHOTOS_DIR = os.path.join(BASE_DIR, 'dados', 'fotos originais')
+SEGMENTED_PHOTOS_DIR = os.path.join(BASE_DIR, 'dados', 'fotos segmentadas')
+OUTPUT_MODEL_DIR = os.path.join(BASE_DIR, 'dados', 'fotos geradas')
 MODELS_DIR = os.path.join(BASE_DIR, 'modelos')
